e5989dfde4ee28b634f216a44b5b840b383d7363
zGuian <guianmarcos32@gmail.com>
2022-12-19 08:18:34 -0300

    Organização v1
    Organização, separado regras de negócios e formulários.

---
+++ CursoWindowsForms/Formularios_Curso 1/Frm_ValidaSenha.cs
@@ -0,0 +1,63 @@
+﻿using CursoWindowsFormsBiblioteca;
+using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_ValidaSenha : Form
+    {
+        bool VerSenhaTxt = false;
+
+        public Frm_ValidaSenha()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Txt_Senha.Text = "";
+            Lbl_Resultado.Text = "";
+        }
+
+        private void Txt_Senha_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            Cls_Uteis.ChecaForcaSenha verifica = new Cls_Uteis.ChecaForcaSenha();
+            Cls_Uteis.ChecaForcaSenha.ForcaDaSenha forca;
+            forca = verifica.GetForcaDaSenha(Txt_Senha.Text);
+            Lbl_Resultado.Text = forca.ToString();
+
+            if (Lbl_Resultado.Text == "Inaceitavel" | Lbl_Resultado.Text == "Fraca")
+            {
+                Lbl_Resultado.ForeColor = Color.Red;
+            }
+            else if (Lbl_Resultado.Text == "Aceitavel")
+            {
+                Lbl_Resultado.ForeColor = Color.Blue;
+            }
+            else
+            {
+                Lbl_Resultado.ForeColor = Color.Green;
+            }
+            
+        }
+
+        private void Btn_VerSenha_Click(object sender, EventArgs e)
+        {
+            if (VerSenhaTxt == false)
+            {
+                Txt_Senha.PasswordChar = '\0';
+                VerSenhaTxt = true;
+                Btn_VerSenha.Text = "Esconder Senha";
+            }
+            else
+            {
+                Txt_Senha.PasswordChar = '*';
+                VerSenhaTxt = false;
+                Btn_VerSenha.Text = "Ver Senha";
+            }
+        }
+    }
+}
